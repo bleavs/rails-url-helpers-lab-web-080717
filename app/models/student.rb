34a4e011ba35f2040007fc83a1c051bf7f351b57
bleavs <bleavitt@colgate.edu>
2017-09-05 18:22:09 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,22 @@
 class Student < ActiveRecord::Base
+
+  after_initialize :set_defaults, unless: :persisted?
+
+  def set_defaults
+    self.active = false if self.active.nil?
+  end
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def status
+    if self.active == true
+      @status = "active"
+    else
+      @status = "inactive"
+    end
+    @status
+  end
+    
 end
